5550d370302e9a885efa1cc1c740fcaa41993d48
Patrick Sy <patrick.sy@get-it.us>
2022-09-23 10:40:09 +0200

    fix metals setup error, api change?

--- dot_config/nvim/lua/metals_setup.lua
+++ dot_config/nvim/lua/metals_setup.lua
@@ -28,7 +28,8 @@ end
 ----------------------------------
 -- global
 vim.opt_global.completeopt = { "menu", "noinsert", "noselect" }
-vim.opt_global.shortmess:remove("F"):append("c")
+vim.opt_global.shortmess:remove("F")
+vim.opt_global.shortmess:append("c")
 
 local default_on_attach = require('lsp_mappings')
 
