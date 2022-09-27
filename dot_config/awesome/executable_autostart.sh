31792a76ba35c83eddd86eaac3625a236382f061
Patrick Sy <patrick.sy@get-it.us>
2022-09-27 17:36:59 +0200

    add compose menu to awesome

--- dot_config/awesome/executable_autostart.sh
+++ dot_config/awesome/executable_autostart.sh
@@ -4,6 +4,9 @@
 ## setup monitors
 xrandr --output DisplayPort-0 --auto --output DVI-D-0 --right-of DisplayPort-0 --auto --output HDMI-A-0 --auto --same-as DisplayPort-0
 
+# set compose key to menu
+setxkbmap -option compose:menu
+
 # start picom
 DISPLAY=":0" picom -b
 
