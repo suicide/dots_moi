4a40de6a59beb0c2dd90dd51249e3e791ce72c1f
Patrick Sy <patrick.sy@get-it.us>
2022-09-27 17:46:55 +0200

    switch nvim config to external repo

--- dot_config/nvim/plugin/theme.vim
+++
@@ -1,9 +0,0 @@
-
-highlight ColorColumn ctermbg=0 guibg=lightgrey
-hi LspReferenceText cterm=bold gui=bold
-hi LspReferenceRead cterm=bold gui=bold
-hi LspReferenceWrite cterm=bold gui=bold
-
-" let g:gruvbox_contrast_dark='hard'
-colorscheme gruvbox
-set background=dark
--- run_pull-git-depedencies.sh
+++ run_pull-git-depedencies.sh
@@ -5,9 +5,9 @@ declare -A repos
 repos=( 
 	["$HOME/.config/awesome/freedesktop"]="git@github.com:lcpz/awesome-freedesktop.git"
 	["$HOME/.config/awesome/lain"]="git@github.com:lcpz/lain.git"
+	["$HOME/.config/nvim"]="git@github.com:suicide/nvim-conf.git"
 	# ["$HOME/.config/tree-sitter-solidity"]="git@github.com:suicide/tree-sitter-solidity.git"
 	["$HOME/.bin/jenv"]="https://github.com/jenv/jenv.git"
-	["$HOME/.bin/vim-plug"]="https://github.com/junegunn/vim-plug.git"
 	["$HOME/.tmux/plugins/tpm"]="https://github.com/tmux-plugins/tpm"
 	)
 
@@ -19,12 +19,9 @@ done
 
 ## create symlinks
 
-mkdir -p $HOME/.local/share/nvim/site/autoload
-ln -sf $HOME/.bin/vim-plug/plug.vim $HOME/.local/share/nvim/site/autoload/plug.vim
-
 # mkdir -p $HOME/.config/nvim/after/queries/solidity
 # ln -sf $HOME/.config/tree-sitter-solidity/queries/highlights.scm $HOME/.config/nvim/after/queries/solidity/highlights.scm
 
 ## build stuff
 
-(cd $HOME/.config/tree-sitter-solidity && npm test)
+# (cd $HOME/.config/tree-sitter-solidity && npm test)
